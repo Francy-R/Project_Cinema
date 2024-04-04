05e5300a3472643ae11ae6097cadbc95e7c40f81
erikagalvisdev <143899498+erikagalvisdev@users.noreply.github.com>
2024-04-04 09:12:23 -0500

    Se crea componente vista Payment contador

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,14 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import './index.css'
-import AppRouter from './routing/AppRouter'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import AppRouter from "./routing/AppRouter";
+import PaymentSummary from "./components/PaymentSummary";
+//import MovieCard from './components/PaymentSummary'
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <AppRouter/>
-  </React.StrictMode>,
-)
+    <PaymentSummary />
+  </React.StrictMode>
+);
+
+//<PaymentSummary/>
