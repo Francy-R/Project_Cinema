de6e8e272ce2ba2fd2371d7885d6478cf2aae15a
erikagalvisdev <143899498+erikagalvisdev@users.noreply.github.com>
2024-04-05 22:29:15 -0500

    Se crea componente PurchaseSummary,estilos y se organizan rutas

--- src/components/TicketsSelection.jsx
+++ src/components/TicketsSelection.jsx
@@ -93,3 +93,4 @@ import React, { useState } from "react";
 //  };
 
 // export default MovieCard;
+
--- src/pages/Tickets.jsx
+++ src/pages/Tickets.jsx
@@ -1,13 +1,15 @@
 import React from 'react'
 import TicketsSelection from '../components/TicketsSelection'
+import PurchaseSummary from '../components/PurchaseSummary'
 
 const Tickets = () => {
   return (
     <div>
       <TicketsSelection/>
+      <PurchaseSummary dataMovie={{titulo: "Spider-manSin Camino a casa ESP", complejo: "Macro Plaza del Mar", fecha: "07 de julio de 2023", funcion: "7:30 PM"}}/>
     </div>
   )
 }
 
 export default Tickets
-//TicketsSelection
+
